404e00baefdca60c3512070d9ee8b2096a91a7dd
Alish Shrestha <cresthaalish@gmail.com>
2025-08-11 17:42:45 +0545

    feat: util function to generate account number

---
+++ core_apps/accounts/utils.py
@@ -0,0 +1,48 @@
+import secrets
+from os import getenv
+from typing import Union, List
+
+from django.db import transaction
+
+from .emails import send_account_creation_email
+from .models import BankAccount
+
+
+def generate_account_number(currency: str) -> str:
+    bank_code = getenv("BANK_CODE")
+    branch_code = getenv("BANK_BRANCH_CODE")
+    
+    currency_codes = {
+        "us_dollar": getenv("CURRENCY_CODE_USD"),
+        "pound_sterling": getenv("CURRENCY_CODE_GBP"),
+        "nepalese_rupees": getenv("CURRENCY_CODE_NP"),
+    }
+    currency_code = currency_codes.get(currency)
+    if not currency_code:
+        raise ValueError(f"Invalid currency: {currency}")
+    
+    prefix = f"{bank_code}{branch_code}{currency_code}"
+
+    remaining_digits = 16 - len(prefix) - 1
+
+    random_digits = "".join(secrets.choice("0123456789") for _ in range(remaining_digits))
+    partial_account_number = f"{prefix}{random_digits}"
+
+    check_digit = calculate_luhn_check_digit(partial_account_number)
+    return f"{partial_account_number}{check_digit}"
+
+
+def calculate_luhn_check_digit(number: str) -> int:
+    def split_into_digits(n: Union[str, int]) -> List[int]:
+        return [int(digit) for digit in str(n)]
+    
+    digits = split_into_digits(number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    total = sum(odd_digits)
+
+    for d in even_digits:
+        doubled = d * 2
+        total += sum(split_into_digits(doubled))
+      
+    return (10 - (total % 10)) % 10
